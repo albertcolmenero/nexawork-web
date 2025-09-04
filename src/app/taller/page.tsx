f25b2b8df04be922ee2325377e87a3bdb07704a9
Albert Colmenero <albert.colmenero@gmail.com>
2025-09-03 20:29:48 -0500

    feat: Add Facebook Pixel tracking and fix project configuration
    - Add Meta Pixel Code to /taller page header for page view tracking
    - Add CompleteRegistration event tracking to thank-you page
    - Remove Turbopack configuration to fix build issues
    - Move favicon.ico to public directory (Next.js app directory structure)
    - Add dev:turbo script for optional Turbopack usage
    - Clean up build configuration and resolve middleware issues

--- src/app/taller/page.tsx
+++ src/app/taller/page.tsx
@@ -3,8 +3,7 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import { motion } from 'framer-motion'
 import { useState, useEffect } from 'react'
-import Head from 'next/head'
-
+import Script from 'next/script'
 import Image from 'next/image'
 
 export default function TallerPage() {
@@ -141,7 +140,7 @@ export default function TallerPage() {
     }
 
     // Inject form after component mounts
-    const timer = setTimeout(injectForm, 100)
+    const timer = setTimeout(injectForm, 500)
     return () => clearTimeout(timer)
   }, [])
 
@@ -532,33 +531,33 @@ export default function TallerPage() {
   return (
     <>
       {/* Meta Pixel Code */}
-      <Head>
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `
-              !function(f,b,e,v,n,t,s)
-              {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-              n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-              if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-              n.queue=[];t=b.createElement(e);t.async=!0;
-              t.src=v;s=b.getElementsByTagName(e)[0];
-              s.parentNode.insertBefore(t,s)}(window, document,'script',
-              'https://connect.facebook.net/en_US/fbevents.js');
-              fbq('init', '1316557306362606');
-              fbq('track', 'PageView');
-            `,
-          }}
+      <Script
+        id="facebook-pixel"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            !function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}(window, document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+            fbq('init', '1316557306362606');
+            fbq('track', 'PageView');
+          `,
+        }}
+      />
+      <noscript>
+        <img
+          height="1"
+          width="1"
+          style={{ display: 'none' }}
+          src="https://www.facebook.com/tr?id=1316557306362606&ev=PageView&noscript=1"
+          alt=""
         />
-        <noscript>
-          <img
-            height="1"
-            width="1"
-            style={{ display: 'none' }}
-            src="https://www.facebook.com/tr?id=1316557306362606&ev=PageView&noscript=1"
-            alt=""
-          />
-        </noscript>
-      </Head>
+      </noscript>
       {/* End Meta Pixel Code */}
 
       <div className="min-h-screen bg-white">
